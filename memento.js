f30d10bd9edb6851312cc8a93fd8c83877b53c3a
Linaoshana <sianadoe@gmail.com>
2020-12-13 17:17:26 +0100

    ok

--- memento.js
+++ memento.js
@@ -1,18 +1,23 @@
-const images = document.querySelectorAll("img");  //tableau des images
-const title = document.querySelector("#title");  //rechercher id title 
-// const tabColor = ["#ED2CD8", "#ED2C58", "#9B3AE7", "#2C97ED"]
+// const images = document.querySelectorAll("img");  //tableau des images
+const title = document.querySelector("#title");
+const container = document.querySelector('#jeu');
+const linkImg = ['mem1.png', 'mem2.png', 'mem3.png', 'mem4.png', 'mem5.png', 'mem6.png'];
+const allImg = [...linkImg, ...linkImg];
 
 
 
+while (allImg.length != 0) {
+    const index = Math.random() * allImg.length << 0;
+    const image = `<div class="imgContainer"><img class="imgBack" src="img_mem/memback.png"><img class="imgFront" src="img_mem/${allImg.splice(index, 1)[0]}"></div>`;
+    container.insertAdjacentHTML("beforeend", image);
+}
 
-//TITRE COULEUR RANDOM AU CLIC IMG 
 
-images.forEach(image => {  
-    image.addEventListener("click", () => {
-    //   alert('hei')
-    let color = '#'+(Math.random()*0xFFFFFF<<0).toString(16);   //<<0 = math.floor
-    title.style.color=color;            // changer style 
-
+const backs = document.querySelectorAll(".imgBack");
+backs.forEach(back => {
+    back.addEventListener('click', () => {
+      back.style.display = 'none';
+      back.nextElementSibling.style.display = 'block';
     });
 });
 
@@ -20,7 +25,25 @@ images.forEach(image => {
 
 
 
-//MEMORY
+// allImg.forEach(link => {  
+//      const image = `<img src="img_mem/${link}">` ;
+//      container.insertAdjacentHTML("beforeend", image);
+// });
+
+
+
+    // link.addEventListener("click", () => {
+
+    // let color = '#'+(Math.random()*0xFFFFFF<<0).toString(16);  math.floor
+    // title.style.color=color;            
+    // });
+
+
+
+
+
+
+
 
 
 

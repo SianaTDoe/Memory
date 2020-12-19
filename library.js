884c477b498a4f3755940d2b9a7dbc96c00f5c73
Linaoshana <sianadoe@gmail.com>
2020-12-19 14:57:43 +0100

    ok

---
+++ library.js
@@ -0,0 +1,37 @@
+export const linkImg = [{
+    front: [
+        "Penguin_mem/penguin1.png",
+        "Penguin_mem/penguin2.png",
+        "Penguin_mem/penguin3.png",
+        "Penguin_mem/penguin4.png",
+        "Penguin_mem/penguin5.png",
+        "Penguin_mem/penguin6.png",
+    ],
+    back: "Penguin_mem/imgbackpengu.png",
+    title : "❄️ ⇝ MEMORY ⇜ ❄️"
+},
+{
+    front: [
+        "img_mem/mem1.png",
+        "img_mem/mem2.png",
+        "img_mem/mem3.png",
+        "img_mem/mem4.png",
+        "img_mem/mem5.png",
+        "img_mem/mem6.png",
+    ],
+    back: "img_mem/memback.png",
+    title : "🪐 ⇝ MEMORY ⇜ 🪐"
+},
+{
+    front: [
+        "Dino_mem/dino1.png",
+        "Dino_mem/dino2.png",
+        "Dino_mem/dino3.png",
+        "Dino_mem/dino4.png",
+        "Dino_mem/dino5.png",
+        "Dino_mem/dino6.png",
+    ],
+    back: "Dino_mem/dino_back.png",
+    title : "🦖 ⇝ MEMORY ⇜ 🦖"
+},
+];
--- memory.js
+++ memory.js
@@ -1,92 +1,133 @@
+import { linkImg } from "./library.js";
+
 const title = document.querySelector("#title");
 const container = document.querySelector('#jeu');
-let result = document.querySelector('#result');
+const result = document.querySelector('#result');
 
-const linkImg = ['mem1.png', 'mem2.png', 'mem3.png', 'mem4.png', 'mem5.png', 'mem6.png'];
-const allImg = [...linkImg, ...linkImg];
+// const linkImg = ['mem1.png', 'mem2.png', 'mem3.png', 'mem4.png', 'mem5.png', 'mem6.png'];
 
-while (allImg.length != 0) {
-    const index = Math.random() * allImg.length << 0;
-    const chemin = allImg.splice(index, 1)[0];
-    const image = `<fieldset class="field1">
-<fieldset class="field2"><div class="imgContainer"><img class="imgBack" src="img_mem/memback.png"><img name="${chemin}" class="imgFront" src="img_mem/${chemin}"></div></fieldset></fieldset>`;
-    container.insertAdjacentHTML('beforeend', image);
-}
+// const saturn = document.querySelector("#saturn");
+// const penguin = document.querySelector("#penguin");
 
-const cartes = document.querySelectorAll('.imgContainer');
-const backs = document.querySelectorAll('.imgBack');
+const themeButtons = document.querySelectorAll(".theme");
+let theme = 1;
+let css = ""
 
-let isSame = null;
-let block = false;
 let score = 0;
 
-backs.forEach(back => {
-    back.addEventListener('click', () => {
-
-        let color = '#' + (Math.random() * 0xFFFFFF << 0).toString(16); //<<0 = math.floor
-        title.style.color = color;
-
-        if (!block) {
-            back.style.display = 'none';
-            back.nextElementSibling.style.display = 'block';
+game();
 
 
-
-            if (isSame == null) {
-                console.log('new compare');
-                isSame = back.nextElementSibling.name;
-            } else if (back.nextElementSibling.name == isSame) {
-                console.log('trouvé');
-                gagne(back);
-            } else {
-                console.log('perdu');
-                perdu(back);
-            }
-        }
-
-
+themeButtons.forEach(themeButton =>
+    themeButton.addEventListener("click", () => {
+      theme = Array.prototype.indexOf.call(themeButtons, themeButton);
+      game();
     })
-})
+);
 
-function gagne(back) {
-    score++;
-    block = true;
-    if (score == linkImg.length) {
-        result.textContent = "Gagné !";
-        const elements = document.querySelectorAll('.imgFront');
-        elements.forEach(element => {
-            element.parentElement.style.visibility = 'visible';
-            element.style.display = 'block';
+function resetGame () {
+    score = 0;
+    while (container.lastElementChild) container.removeChild(container.lastElementChild);
+}
+// saturn.addEventListener('click', () => {
+//     theme = 0;
+//     game();
+// });
+
+// penguin.addEventListener('click', () => {
+//     theme = 1;
+//     game();
+// });
+
+
+// const allImg = [...linkImg[0], ...linkImg[0]];
+
+function game() {
+    resetGame();
+
+    const allImg = [...linkImg[theme], ...linkImg[theme]];
+
+    while (allImg.length != 0) {
+        const index = Math.random() * allImg.length << 0;
+        const chemin = allImg.splice(index, 1)[0];
+        const image = `<fieldset class="field1">
+<fieldset class="field2"><div class="imgContainer"><img class="imgBack" src="img_mem/memback.png"><img name="${chemin}" class="imgFront" src="${chemin}"></div></fieldset></fieldset>`;
+        container.insertAdjacentHTML('beforeend', image);
+    }
+
+    const cartes = document.querySelectorAll('.imgContainer');
+    const backs = document.querySelectorAll('.imgBack');
+
+    let isSame = null;
+    let block = false;
+
+    backs.forEach(back => {
+        back.addEventListener('click', () => {
+
+            let color = '#' + (Math.random() * 0xFFFFFF << 0).toString(16); //<<0 = math.floor
+            title.style.color = color;
+
+            if (!block) {
+                back.style.display = 'none';
+                back.nextElementSibling.style.display = 'block';
+
+
+
+                if (isSame == null) {
+                    console.log('new compare');
+                    isSame = back.nextElementSibling.name;
+                } else if (back.nextElementSibling.name == isSame) {
+                    console.log('trouvé');
+                    gagne(back);
+                } else {
+                    console.log('perdu');
+                    perdu(back);
+                }
+            }
+
+
         })
-    } else {
+    })
+
+    function gagne(back) {
+        score++;
+        block = true;
+        if (score == linkImg[theme].length) {
+            result.textContent = "Gagné !";
+            const elements = document.querySelectorAll('.imgFront');
+            elements.forEach(element => {
+                element.parentElement.style.visibility = 'visible';
+                element.style.display = 'block';
+            })
+        } else {
+            setTimeout(() => {
+                const elements = document.querySelectorAll(`img[name='${isSame}']`);
+                elements.forEach(element => element.parentElement.style.visibility = 'hidden')
+
+                back.parentElement.style.visibility = 'hidden';
+
+                isSame = null;
+                block = false;
+                result.textContent = "Génial !";
+            }, 1000)
+        }
+    }
+
+    function perdu(back) {
+        block = true;
         setTimeout(() => {
             const elements = document.querySelectorAll(`img[name='${isSame}']`);
-            elements.forEach(element => element.parentElement.style.visibility = 'hidden')
+            elements.forEach(element => {
+                element.previousSibling.style.display = 'block';
+                element.style.display = 'none';
+            })
 
-            back.parentElement.style.visibility = 'hidden';
+            back.style.display = 'block';
+            back.nextElementSibling.style.display = 'none';
 
             isSame = null;
             block = false;
-            result.textContent = "Génial !";
-        }, 1500)
+            result.textContent = "Essaye encore !";
+        }, 1000)
     }
 }
-
-function perdu(back) {
-    block = true;
-    setTimeout(() => {
-        const elements = document.querySelectorAll(`img[name='${isSame}']`);
-        elements.forEach(element => {
-            element.previousSibling.style.display = 'block';
-            element.style.display = 'none';
-        })
-
-        back.style.display = 'block';
-        back.nextElementSibling.style.display = 'none';
-
-        isSame = null;
-        block = false;
-        result.textContent = "Essaye encore !";
-    }, 1500)
-}
-

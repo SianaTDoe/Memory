620390a8d79605e99c4095c222257a0c5e0a2189
Linaoshana <sianadoe@gmail.com>
2020-12-19 15:37:17 +0100

    ok

--- memory.js
+++ memory.js
@@ -5,11 +5,6 @@ const title = document.querySelector("#title");
 const container = document.querySelector('#jeu');
 const result = document.querySelector('#result');
 
-// const linkImg = ['mem1.png', 'mem2.png', 'mem3.png', 'mem4.png', 'mem5.png', 'mem6.png'];
-
-// const saturn = document.querySelector("#saturn");
-// const penguin = document.querySelector("#penguin");
-
 const themeButtons = document.querySelectorAll(".theme");
 let theme = 1;
 let css = "saturn";
@@ -31,18 +26,6 @@ function resetGame () {
     score = 0;
     while (container.lastElementChild) container.removeChild(container.lastElementChild);
 }
-// saturn.addEventListener('click', () => {
-//     theme = 0;
-//     game();
-// });
-
-// penguin.addEventListener('click', () => {
-//     theme = 1;
-//     game();
-// });
-
-
-// const allImg = [...linkImg[0], ...linkImg[0]];
 
 function game() {
     resetGame();
